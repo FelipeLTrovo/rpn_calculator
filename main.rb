f835d56c72cc3b389891de1b1a59370c37cdfbc6
FelipeLTrovo <felipeltrovo@hotmail.com>
2022-12-19 22:24:59 -0300

    Driver class tweaks.

--- main.rb
+++ main.rb
@@ -2,13 +2,14 @@ require_relative './commands/rpn_calculator'
 require 'colorize'
 
 
-puts "\nPlease type an operand or an operator: ".blue
-puts "\nGet the answer to your expression by inserting the equal symbol '='".blue
-param = ''
 while true do
+  puts "\nPlease insert a postfix expression: ".blue
+  puts "\nEach operand/operator must be separated by an empty space(Space key) or line break(Enter key)".yellow
+  puts "\nGet the answer to your expression by inserting the equal symbol '='".yellow
+  param = ''
   param << gets("=")
   cmd = RpnCalculator.new(param).call
-  puts "\Expression: #{param}".blue
+  puts "\Expression: #{param.chars.join('')}".yellow
   puts "\nResult: #{cmd}".green
   puts "\nPress Ctrl + C to exit the program.".red
 end
